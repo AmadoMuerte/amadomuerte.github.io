a7b46f8c2d0013807120b6dc07ff41d9fb6b26c4
Amado <kisik854@gmail.com>
2022-08-19 21:51:45 +1000

    build

---
+++ build/precache-manifest.fb203029dc155a2b0bc62fb2790aca0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5424919d1482bb5cc84",
+    "url": "/static/css/main.64529d79.chunk.css"
+  },
+  {
+    "revision": "b5424919d1482bb5cc84",
+    "url": "/static/js/main.b5424919.chunk.js"
+  },
+  {
+    "revision": "ffac0852b135a7b31abd",
+    "url": "/static/js/1.ffac0852.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac52b8f72445aa8dbd9a5c6cebdcdab1",
+    "url": "/static/media/bookmark-solid.ac52b8f7.svg"
+  },
+  {
+    "revision": "412dbb692cab57e5de014f6f462a4d61",
+    "url": "/static/media/square-plus-solid.412dbb69.svg"
+  },
+  {
+    "revision": "0fd2f74ccfa2337f3c31441b6a51bc30",
+    "url": "/index.html"
+  }
+];
